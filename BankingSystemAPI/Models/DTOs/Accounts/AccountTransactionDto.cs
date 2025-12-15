9443fb6c80678c7e80227f965eaadf03cd0d83cb
Xuan Hoang Ha <hoanghaxuan2434@gmail.com>
2025-12-15 11:55:28 -0500

    Update detail

---
+++ BankingSystemAPI/Models/DTOs/Accounts/AccountTransactionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystemAPI.Models.DTOs.Accounts
+{
+    public class AccountTransactionDto
+    {
+        public DateTime Date { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
