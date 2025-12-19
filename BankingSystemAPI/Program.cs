b7ac0d45b4a33004be0f6be44cd9520db6e2d2a6
Xuan Hoang Ha <hoanghaxuan2434@gmail.com>
2025-12-19 10:10:42 -0500

    Change db to SqlServer

--- BankingSystemAPI/Program.cs
+++ BankingSystemAPI/Program.cs
@@ -22,7 +22,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Options
 builder.Services.Configure<PasswordOptions>(
